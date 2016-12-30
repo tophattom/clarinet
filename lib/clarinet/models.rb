bb8504241cdc9ff2929315f268907ee8276e7d40
Jaakko Rinta-Filppula <jaakko@flockler.com>
2016-12-30 15:45:01 +0200

    Implement searching for models

--- lib/clarinet/models.rb
+++ lib/clarinet/models.rb
@@ -26,7 +26,11 @@ module Clarinet
 
       return Clarinet::Model.new @app, model_data if model_data['id']
 
-      # TODO: search for a model in the API if no id is given
+      search_results = search model_data['name'], model_data['type']
+
+      return search_results.find { |result| result.model_version.id == model_data['version'] }.first if model_data['version']
+
+      search_results.first
     end
 
     def predict(model, inputs)
@@ -40,6 +44,29 @@ module Clarinet
         url,
         headers: @app.auth_header,
         query: options
+      )
+
+      data = response.parsed_response
+
+      Clarinet::Utils.check_response_status data['status']
+
+      Clarinet::Models.new @app, data['models']
+    end
+
+    def search(name, type = nil)
+      url = 'https://api.clarifai.com/v2/models/searches'
+
+      params = {
+        model_query: {
+          name: name,
+          type: type
+        }
+      }
+
+      response = HTTParty.post(
+        url,
+        headers: @app.auth_header.merge('Content-Type' => 'application/json'),
+        body: params.to_json
       )
 
       data = response.parsed_response
