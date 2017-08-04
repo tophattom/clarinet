1889b670c8720a25bdccb713004efad9678f9c89
Jaakko Rinta-Filppula <jaakko@flockler.com>
2017-08-04 10:23:42 +0300

    Add concepts accessor to Clarinet::App

--- lib/clarinet/app.rb
+++ lib/clarinet/app.rb
@@ -4,11 +4,13 @@ module Clarinet
   class App
 
     attr_reader :client
+    attr_reader :concepts
     attr_reader :models
 
     def initialize(api_key)
       @client = Clarinet::Client.new api_key
 
+      @concepts = Clarinet::Concepts.new self
       @models = Clarinet::Models.new self
     end
 
